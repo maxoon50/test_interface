73ca5601b635dd2cc8149e982f8aac32ab193538
maxoon <desfouxmaxime@gmail.com>
2018-08-06 16:15:56 +0200

    add  : mixins

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -15,7 +15,7 @@
         <div class="grow border">
             <Contents ref="extras" :categorie="'extras'"/>
         </div> -->
-      <template v-for="child in componentList">
+      <template v-for="child in eltsName">
             <component :is="'contents'" :key="child" :categorie="'contents'" ref="contents"></component>
         </template>
     </main>
@@ -24,61 +24,59 @@
 <script>
     import Contents from './subComponents/Contents';
     import { EventBus } from '../main';
-
+    import { mixinEletWithChild } from '../mixins/mixinEletWithChild';
     export default {
         name: "app-main",
         components: {
             Contents
         },
+        mixins : [mixinEletWithChild],
         data:function(){
             return {
-                componentList: ['channels', 'contents', 'apps', 'films', 'extras'],
+                eltsName: ['channels', 'contents', 'apps', 'films', 'extras'],
                 focused: false,
                 focus: 0,
                 lastFocused: null
             }
         },
         methods: {
-            isFocus: function () {
-                this.focused = true;
-                this.getFocus();
-                EventBus.$emit('parentRemoveListener');
-            },
-            removeFocus: function () {
-                this.focused = false;
-                this.removeListeners();
-                this.lastFocused.removeFocus();
-                //remove tous les focus des enfants
-            },
-            getFocus: function () {
-                this.focus = 0;
-                this.initListeners();
-                this.giveFocus();
-            },
-            setFocus: function (pos) {
-                if(this.focus <= 0 && pos == -1){
-                    this.focus = this.componentList.length-1;
-                }else if(this.focus == this.componentList.length-1 && pos == 1){
-                    this.focus = 0;
-                }else{
-                    this.focus += pos;
-                }
-                this.giveFocus();
-            },
-            giveFocus: function () {
-                if(this.lastFocused != null){
-                    this.lastFocused.removeFocus();
-                }
-                let eltToFocus = this.$refs.contents[this.focus];
-                eltToFocus.isFocus();
-                this.lastFocused = eltToFocus;
-            },
-            initListeners: function () {
-                window.addEventListener('keydown', this.listener );
-            },
-            removeListeners: function() {
-                window.removeEventListener('keydown', this.listener);
-            },
+       
+            // removeFocus: function () {
+            //     this.focused = false;
+            //     this.removeListeners();
+            //     this.lastFocused.removeFocus();
+            //     //remove tous les focus des enfants
+            // },
+            // getFocus: function () {
+            //     this.focus = 0;
+            //     this.initListeners();
+            //     this.giveFocus();
+            // },
+            // setFocus: function (pos) {
+               
+            //     if(this.focus <= 0 && pos == -1){
+            //         this.focus = this.componentList.length-1;
+            //     }else if(this.focus == this.componentList.length-1 && pos == 1){
+            //         this.focus = 0;
+            //     }else{
+            //         this.focus += pos;
+            //     }
+            //     this.giveFocus();
+            // },
+            // giveFocus: function () {
+            //     if(this.lastFocused != null){
+            //         this.lastFocused.removeFocus();
+            //     }
+            //     let eltToFocus = this.$refs.contents[this.focus];
+            //     eltToFocus.isFocus();
+            //     this.lastFocused = eltToFocus;
+            // },
+            // initListeners: function () {
+            //     window.addEventListener('keydown', this.listener );
+            // },
+            // removeListeners: function() {
+            //     window.removeEventListener('keydown', this.listener);
+            // },
             listener: function ({code}) {
              {
                     switch(code){
---
+++ src/mixins/mixinEletWithChild.js
@@ -0,0 +1,52 @@
+import { EventBus } from '../main';
+export const mixinEletWithChild = {
+    data: function() {
+        return {
+            focused: false,
+            componentList : this.$refs
+        }
+    },
+    methods:{
+        isFocus: function () {
+            this.focused = true;
+            this.getFocus();
+           // EventBus.$emit('parentRemoveListener');
+        },
+        removeFocus: function () {
+            this.focused = false;
+            this.removeListeners();
+            this.lastFocused.removeFocus();
+            //remove tous les focus des enfants
+        },
+        getFocus: function () {
+            this.focus = 0;
+            this.initListeners();
+            this.giveFocus();
+        },
+        
+        setFocus: function (pos) {
+            if(this.focus <= 0 && pos == -1){
+                this.focus = this.componentList.length-1;
+            }else if(this.focus == this.componentList.length-1 && pos == 1){
+                this.focus = 0;
+            }else{
+                this.focus += pos;
+            }
+            this.giveFocus();
+        },
+        giveFocus: function () {
+            if(this.lastFocused != null){
+                this.lastFocused.removeFocus();
+            }
+            let eltToFocus = this.$refs.contents[this.focus];
+            eltToFocus.isFocus();
+            this.lastFocused = eltToFocus;
+        },
+        initListeners: function () {
+            window.addEventListener('keydown', this.listener );
+        },
+        removeListeners: function() {
+            window.removeEventListener('keydown', this.listener);
+        },
+    }
+}
